1697a937a4994ba2bc073e1e2347a4415d77051e
Dennis Kiboi <mururi.kiboi@gmail.com>
2022-03-12 19:32:52 +0300

    Add user profile and edit profile routes

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,9 @@
-from flask import render_template
+from flask import render_template, redirect, url_for, abort
 from . import main
+from ..models import User
+from .forms import UpdateProfile
+from .. import db
+from flask_login import login_required, current_user
 
 # Views
 @main.route('/')
@@ -7,3 +11,32 @@ def index():
     title = 'This is Me'
 
     return render_template('index.html', title=title)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    return render_template('profile/update.html',form =form)
--- config.py
+++ config.py
@@ -5,6 +5,16 @@ class Config:
     General configuration parent class
     '''
     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://mururi:sqlpass@localhost/blog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     '''
